983e3d8002ee19b27d8c584db9b4d5bb34b23b4c
codeclanstudent <sstewart1993@outlook.com>
2020-11-24 14:44:53 +0000

    Initial commit

---
+++ src/main/java/com/example/codeclan/One2ManyHW/One2ManyHwApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.One2ManyHW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class One2ManyHwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(One2ManyHwApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/One2ManyHW/One2ManyHwApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.One2ManyHW;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class One2ManyHwApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
